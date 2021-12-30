baf16a891ac3adfc027b8d91e01f87cff0303ede
LÊ HỮU HOÀN <89791546+Hoanle396@users.noreply.github.com>
2021-12-30 10:46:59 +0700

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -24,7 +24,7 @@ class IndexController extends Controller
        $feedback=new Feedback();
        $feedback->fill($request->all());
        $feedback->save();
-       Mail::to($request->email)->queue(new sendmail());
+//        Mail::to($request->email)->queue(new sendmail());
        return response()->json(['message'=>'phản hồi thành công']);
     }
     public function  addSkill(AddSkillRequest $request){
